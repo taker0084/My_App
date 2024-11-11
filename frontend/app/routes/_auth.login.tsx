7f0ad9ad0ca277097c8f5d2e14bb57a4336084f5
Takeru Seki <0084takeru@gmail.com>
2024-11-11 16:18:27 +0900

    ログイン機構の作成

---
+++ frontend/app/routes/_auth.login.tsx
@@ -0,0 +1,40 @@
+import { ActionFunctionArgs, redirect } from "@remix-run/node";
+import { useActionData } from "@remix-run/react";
+
+export const loader = async () => {
+  //return redirect("/");
+  return null;
+};
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  const formObject = Object.fromEntries(formData.entries());
+  const response = await fetch("http://localhost:5000/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formObject), // オブジェクトをJSONに変換して送信
+  });
+  if (response.ok) return redirect(`/user/${formData.get("name")}`);
+  else {
+    const error = response.text;
+    return error;
+  }
+};
+export default function Login() {
+  const error = useActionData<typeof action>();
+  return (
+    <div>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <h2>Log In</h2>
+      <form method="post">
+        <label htmlFor="name">Email</label>
+        <input type="text" name="name" id="name" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <button type="submit">LogIn</button>
+      </form>
+    </div>
+  );
+}
